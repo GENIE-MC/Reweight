90f5932271b51fb8f5e818080dcc930abb7b359d
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2023-09-20 13:02:18 +0100

    Initialize GReWeightProfessor with comparsion XML

--- src/RwCalculators/GReWeightProfessor.cxx
+++ src/RwCalculators/GReWeightProfessor.cxx
@@ -1,6 +1,9 @@
 #include "RwCalculators/GReWeightProfessor.h"
 #include "Framework/Messenger/Messenger.h"
+#include "Framework/Utils/XmlParserUtils.h"
+#include <cstdlib>
 #include <fstream>
+#include <memory>
 #include <sstream>
 #include <string>
 
@@ -34,13 +37,11 @@ double GReWeightProfessor::CalcWeight(const genie::EventRecord &event) {
   return observable_splines->GetRatio(event, systematics_values, orig_value);
 }
 
-void GReWeightProfessor::Initialize(std::string spline_filepath) {
-  observable_splines = std::make_unique<ObservableSplines>();
-  ReadProf2Spline(spline_filepath);
-  std::string observable_name = "genie::rew::ObservableMuonMomentum";
-  observable_splines->InitializeObservable(observable_name);
-  std::vector<std::vector<double>> binning{};
-  observable_splines->InitializeBins(binning);
+void GReWeightProfessor::Initialize(std::string conf_file) {
+  // This is a very simliar function to the one in
+  // GENIE_COMPARISONS/src/Observables/GeneralReweightObs.cxx
+  // we may consider merging them in the future
+  ReadComparionConf(conf_file);
 }
 
 void GReWeightProfessor::ReadProf2Spline(std::string filepath) {
@@ -83,7 +84,51 @@ void GReWeightProfessor::ReadProf2Spline(std::string filepath) {
   observable_splines->InitializeIpols(var_lines);
 }
 
-void GReWeightProfessor::ReadComparionConf(std::string filepath) {}
+void GReWeightProfessor::ReadComparionConf(std::string conf_file) {
+  xmlDocPtr xmldoc = xmlParseFile(conf_file.c_str());
+  if (xmldoc == NULL) {
+    LOG("GReWeightProfessor::Initialize", pERROR)
+        << "Error parsing file " << conf_file << "\n";
+    exit(1);
+  }
+  dimension = utils::xml::GetInt(xmldoc, "config/observables/dimension");
+  std::vector<std::vector<double>> bin_edges{};
+  bin_edges.resize(dimension);
+  // we want to reference the bin edge file relative to the GENIE_COMPARISONS
+  const char *basedir = std::getenv("GENIE_COMPARISONS");
+  basedir = basedir ? basedir : "";
+  size_t total_bins = 1;
+  for (size_t i = 0; i < dimension; ++i) {
+    auto nodepath =
+        "config/observables/bin_edges/bin_edge_" + std::to_string(i + 1);
+    std::string bin_edge_filepath = utils::xml::GetString(xmldoc, nodepath);
+    bin_edge_filepath = utils::str::TrimSpaces(bin_edge_filepath);
+    auto filepath = (bin_edge_filepath[0] != '/')
+                        ? (std::string{basedir} + "/" + bin_edge_filepath)
+                        : bin_edge_filepath;
+    // LOG("", pINFO) << "reading from " << filepath;
+    
+    LOG("GReWeightProfessor", pINFO)
+        << "Reading bin edges from " << filepath;
+    std::ifstream bin_edge_file(filepath);
+
+    std::string line;
+    while (std::getline(bin_edge_file, line)) {
+      bin_edges[i].push_back(std::stod(line));
+    }
+    total_bins *= bin_edges[i].size() - 1;
+  }
+  auto obsname = genie::utils::xml::GetString(xmldoc, "config/observable_name");
+  auto disc_bin_names =
+      genie::utils::xml::GetString(xmldoc, "config/enabled_discrete_bins");
+  auto disc_bin_names_vec = utils::str::Split(disc_bin_names, ",");
+  observable_splines = std::make_unique<rew::ObservableSplines>();
+  observable_splines->InitializeBins(bin_edges);
+  observable_splines->InitializeObservable(obsname);
+  observable_splines->InitializeDiscreteBins(disc_bin_names_vec);
+  total_bins *= observable_splines->GetNChannel();
+  LOG("GReWeightProfessor", pINFO) << "Initized with " << total_bins << "bins";
+}
 
 GReWeightProfessor::GReWeightProfessor(std::string name)
     : GReWeightModel(name) {}
