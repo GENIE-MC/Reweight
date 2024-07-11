3e8db9bc9860900fe451f7215814da6fe5930d3d
larsb-p <lars@physik.tu-berlin.de>
2024-07-11 06:30:05 +0200

    Added line â#include Framework/GHEP/GHepParticle.h to src/RwCalculators/GReWeightINuke.h

--- src/RwCalculators/GReWeightINuke.h
+++ src/RwCalculators/GReWeightINuke.h
@@ -34,6 +34,8 @@
 
 //#define _G_REWEIGHT_INUKE_DEBUG_NTP_
 
+#include "Framework/GHEP/GHepParticle.h"
+
 // GENIE/Reweight includes
 #include "RwCalculators/GReWeightModel.h"
 #include "RwCalculators/GReWeightINukeParams.h"
