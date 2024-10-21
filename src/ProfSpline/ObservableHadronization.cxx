b07d8191aa11aba75ffab30e9b62377d725bf5ce
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2024-10-21 21:59:09 +0800

    use pion momentum in rest frame

--- src/ProfSpline/ObservableHadronization.cxx
+++ src/ProfSpline/ObservableHadronization.cxx
@@ -25,14 +25,27 @@ KinematicVariables ObservableHadronization::CalcKinematicVariables(
   ret.push_back(W);
 
   auto np = event.GetEntriesFast();
+  TLorentzVector had_system{};
+  for (int i{}; i < np; i++) {
+    auto particle = event.Particle(i);
+    if (particle->Status() == kIStStableFinalState) {
+      had_system += *particle->P4();
+    }
+  }
+
+  auto had_system_boost = had_system.BoostVector();
+
   double max_p_pion{};
   for (int i{}; i < np; i++) {
     auto particle = event.Particle(i);
     if (particle->Status() == kIStStableFinalState &&
         (particle->Pdg() == 211 || particle->Pdg() == -211 ||
          particle->Pdg() == 111)) {
-      auto p = particle->P4()->P();
-      max_p_pion = std::max(max_p_pion, p);
+      // auto p = particle->P4()->P();
+      // max_p_pion = std::max(max_p_pion, p);
+      auto p_in_had_rest_frame = *(particle->P4());
+      p_in_had_rest_frame.Boost(-had_system_boost);
+      max_p_pion = std::max(max_p_pion, p_in_had_rest_frame.P());
     }
   }
   if (max_p_pion == 0) {
